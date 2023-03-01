683cb80809840f5e5e1a8b9f7460329ef55cc0d9
Arthur <martinsarthur07@gmail.com>
2023-03-01 15:37:07 -0300

    feat: test environment initial settings

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,2 +1,2 @@
 // https://github.com/testing-library/jest-dom#usage
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+// https://github.com/luizomf/vitest-vite-and-react-boilerplate
 export default defineConfig({
   plugins: [react()],
-})
+  root: 'src',
+  build: {
+    outDir: '../dist',
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./tests/setup.js'],
+    include: ['**/*(*.)?{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+  },
+});
