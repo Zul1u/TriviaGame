bc5f432d1edf8818268781849af8aca911e018ac
Arthur <martinsarthur07@gmail.com>
2023-03-01 20:03:35 -0300

    fix: vit.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,10 @@ import react from '@vitejs/plugin-react';
 // https://github.com/luizomf/vitest-vite-and-react-boilerplate
 export default defineConfig({
   plugins: [react()],
-  root: 'src',
-  build: {
-    outDir: '../dist',
-  },
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./tests/setup.js'],
+    setupFiles: ['./src/tests/setup.js'],
     include: ['**/*(*.)?{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
 });
