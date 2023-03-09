4cbf07d0843aae3b35e0bb5f5ba6cce6070f898f
Arthur <martinsarthur07@gmail.com>
2023-03-09 20:35:01 -0300

    feat: implements the StartGameModal component and the Button component

--- src/page/Scoreboard.jsx
+++ src/page/Scoreboard.jsx
@@ -1,13 +1,18 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Button from '../components/Button';
 import Header from '../components/Header';
 import PlayerReportCard from '../components/PlayerReportCard';
+import StartGameModal from '../components/StartGameModal';
 import { deletePlayerInfoStorage } from '../helpers/localStorage/playerInfoStorage';
 import { getScoreboardStorage } from '../helpers/localStorage/scoreboardStorage';
 
 export default function Scoreboard() {
   const [scoreboard, setScoreboard] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+  const [inputValue, setInputValue] = useState('');
   const navigate = useNavigate();
+  const randomNumber = () => Math.floor(Math.random() * 100);
 
   useEffect(() => {
     const sortedScoreboard = getScoreboardStorage().sort((a, b) => b.score - a.score);
@@ -20,18 +25,27 @@ export default function Scoreboard() {
   };
 
   return (
-    <div>
+    <>
       <Header title="Scoreboard" />
-      <section className="playerReport-card-container">
-        <ul className="playerReport-list">
+      <div className="scoreboard-btn-container">
+        <Button handleClick={handleClick}>Go to Home</Button>
+        <Button handleClick={() => setOpenModal(true)}>Play Again</Button>
+        <StartGameModal
+          openModal={openModal}
+          setOpenModal={setOpenModal}
+          setInputValue={setInputValue}
+          inputValue={inputValue}
+        />
+      </div>
+      <section className="player-report-card-container">
+        <ul className="player-report-list">
           {scoreboard.length > 0 && scoreboard.map((player) => (
-            <li key={player.score * scoreboard.length}>
+            <li key={player.score * randomNumber()}>
               <PlayerReportCard playerReport={player} />
             </li>
           ))}
         </ul>
       </section>
-      <button type="button" onClick={handleClick}>Go to home</button>
-    </div>
+    </>
   );
 }
